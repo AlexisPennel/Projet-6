2328bf0bd6969b2d6b94f45818ade3e1e8763fa2
Alexis Pennel <alexispennel@gmail.com>
2023-01-17 14:31:41 +0100

    controller modification sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -31,10 +31,26 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.updateSauce = (req, res, next) => {
-    res.status(201).json({
-        message: `Maj de la sauce avec l'id fourni`
-    });
-};
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete sauceObject._userId;
+    Sauces.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Sauces.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+ };
 
 exports.deleteSauce = (req, res, next) => {
     res.status(200).json({
