5547b1e75f503b0bc3543314497ede9ae6af4581
Alexis Pennel <alexispennel@gmail.com>
2023-01-18 16:55:40 +0100

    retrait dossier images github

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const saucesRoutes = require('./routes/sauces');
 
 mongoose.set('strictQuery', true);
 
+// Création app express et connexion MongoDB
 const app = express();
 mongoose.connect('mongodb+srv://AlexP:1234@clusterp6.w6swi9g.mongodb.net/?retryWrites=true&w=majority',
     {
@@ -16,8 +17,10 @@ mongoose.connect('mongodb+srv://AlexP:1234@clusterp6.w6swi9g.mongodb.net/?retryW
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+// Prise en charge JSON
 app.use(express.json());
 
+// Headers pour permettre des requêtes cross-origin  
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -25,6 +28,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// Endpoints
 app.use('/api/auth', authRoutes);
 
 app.use('/api/sauces', saucesRoutes);
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -15,15 +15,6 @@ exports.signUp = (req, res, next) => {
                 .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
-
-
-    // const user = new User({
-    //  ...req.body
-    // });
-    // console.log(user);
-    // user.save()
-    //  .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-    //  .catch(error => res.status(400).json({ error }));
 };
 
 exports.login = (req, res, next) => {
@@ -52,7 +43,3 @@ exports.login = (req, res, next) => {
 };
 
 
-    // console.log(req.body);
-    // res.status(200).json({
-    //     message: `Utilisateur ${req.body.email} connecté`
-    // });
