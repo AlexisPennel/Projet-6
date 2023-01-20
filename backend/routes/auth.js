74ddc6a541f34081e8532dd244ac90a4c3827d8d
Alexis Pennel <alexispennel@gmail.com>
2023-01-20 16:35:03 +0100

    express limiter

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const authCtrl = require('../controllers/auth');
+const limiter = require('../middleware/loginLimiter');
 const { body } = require('express-validator');
 
 
-router.post('/signup', [
-    body('email', 'Email invalide')
-        .trim()
-        .isEmail()
-        .normalizeEmail(),
-    body('password', "Mot de passe incorrect")
-        .trim()
-        .isStrongPassword()
-],
+router.post('/signup',
+    limiter,
+    [
+        body('email', 'Email invalide')
+            .trim()
+            .isEmail()
+            .normalizeEmail(),
+        body('password', "Mot de passe incorrect")
+            .trim()
+            .isStrongPassword()
+    ],
     authCtrl.signUp);
 
-router.post('/login', authCtrl.login);
+router.post('/login',
+    limiter,
+    authCtrl.login);
 
 module.exports = router;
